984adc7150306c1274df144a762836e1042fbb91
jahnavi <jahnavipatel0206@gmail.com>
2022-03-04 19:30:01 +0530

    procfile added

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-3ne1%q23%$o0p_3hv@ry*s4)dz6f-4(ri)*8n5+8ex$%)f=exc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['boiling-chamber-03286.herokuapp.com','127.0.0.1']
 
 
 # Application definition
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'home',
-    'notes'
+    'notes',
 ]
 
 MIDDLEWARE = [
